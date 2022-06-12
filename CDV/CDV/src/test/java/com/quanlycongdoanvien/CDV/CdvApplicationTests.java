08ea82d474703c20c72154764be6ace374a37f66
tienbaovuong <tienbaoams@gmail.com>
2022-06-12 14:15:34 +0700

    init springboot

---
+++ CDV/CDV/src/test/java/com/quanlycongdoanvien/CDV/CdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanlycongdoanvien.CDV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
